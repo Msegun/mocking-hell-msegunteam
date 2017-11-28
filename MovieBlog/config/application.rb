599fbdebfc12bf488312b5e9d7c7b01213c4b9be
msegun <msegun@localhost.localdomain>
2017-11-28 10:44:24 +0100

    New BlogApplication

--- MovieBlog/config/application.rb
+++ MovieBlog/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Filmweblog
+module MovieBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
