cdbba0471f448d3f6a17b091cfe8ed6b8eaa9863
msegun <msegun@localhost.localdomain>
2017-11-28 12:13:08 +0100

    Rails app added a few backend tutorial thingies still studying

---
+++ MovieBlog/app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+
+  # Show all articles
+  def index
+    @articles = Article.all
+  end
+
+  # Show one article they are beeing found by ID parameter and beeing shown in app/views/articles/show.html.erb
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  # Creating a new article with parameters Text and Title placed and permited in private article_params
+  def create
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+      params.require(:article).permit(:title, :text)
+  end
+end
