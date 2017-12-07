888d3fd81e702d6f96ade30d40ef054af66a8530
msegun <msegun@localhost.localdomain>
2017-12-07 11:44:05 +0100

    Added comments controlles and other thingies

--- MovieBlog/app/models/article.rb
+++ MovieBlog/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   # it checks wether or not the title is minimum five letters long
   validates :title, presence: true, length: { minimum: 5 }
 end
