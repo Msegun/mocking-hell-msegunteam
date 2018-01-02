e5e89666f45b96f4f77c2edd6200748f5bfd292e
msegun <msegun@localhost.localdomain>
2018-01-02 12:12:30 +0100

    Hiding links from non signedin users & non owners

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -4,17 +4,31 @@ RSpec.feature "Article-show" do
 
   feature "#Showing an Article" do
     before do
-      john = User.create(email: "john@example.com", password: "password")
-      login_as(john)
-      @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: john)
+      @john = User.create(email: "john@example.com", password: "password")
+      @fred = User.create(email: "fred@example.com", password: "password")
+      @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: @john)
     end
 
-    scenario "A user lists all articles" do
-    visit "/"
-    click_link @article.title
-    expect(page).to have_content(@article.title)
-    expect(page).to have_content(@article.body)
-    expect(current_path).to eq(article_path(@article))
+    scenario "to a non-signed in user hide Edit/Delete Links" do
+      visit "/"
+      click_link @article.title
+      expect(page).to have_content(@article.title)
+      expect(page).to have_content(@article.body)
+      expect(current_path).to eq(article_path(@article))
+      expect(page).not_to have_link("Edit Article")
+      expect(page).not_to have_link("Delete Article")
+    end
+
+    scenario "to a non-owner hide Edit/Delete Links" do
+      login_as(@fred)
+      visit "/"
+      click_link @article.title
+      
+      expect(page).to have_content(@article.title)
+      expect(page).to have_content(@article.body)
+      expect(current_path).to eq(article_path(@article))
+      expect(page).not_to have_link("Edit Article")
+      expect(page).not_to have_link("Delete Article")
     end
   end
 end
