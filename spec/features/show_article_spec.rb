99be743850c7c86c5da4afef1a325129f4a2e5ec
msegun <msegun@localhost.localdomain>
2018-01-02 12:15:26 +0100

    Finished hiding links

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -23,12 +23,24 @@ RSpec.feature "Article-show" do
       login_as(@fred)
       visit "/"
       click_link @article.title
-      
+
       expect(page).to have_content(@article.title)
       expect(page).to have_content(@article.body)
       expect(current_path).to eq(article_path(@article))
       expect(page).not_to have_link("Edit Article")
       expect(page).not_to have_link("Delete Article")
+    end
+
+    scenario "A sign in owner sees both Edit/Delete Links" do
+      login_as(@john)
+      visit "/"
+      click_link @article.title
+
+      expect(page).to have_content(@article.title)
+      expect(page).to have_content(@article.body)
+      expect(current_path).to eq(article_path(@article))
+      expect(page).to have_link("Edit Article")
+      expect(page).to have_link("Delete Article")
     end
   end
 end
