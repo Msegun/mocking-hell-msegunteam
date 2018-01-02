478d347adcdafedee32438b9947ef0c905500270
msegun <msegun@localhost.localdomain>
2018-01-02 10:54:53 +0100

    Updating specs for PHD to be happy

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
-RSpec.feature "Showing an Article" do
+RSpec.feature "Article" do
 
-  before do
-    john = User.create(email: "john@example.com", password: "password")
-    login_as(john)
-    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: john)
-  end
+feature "#Showing an Article" do
+    before do
+      john = User.create(email: "john@example.com", password: "password")
+      login_as(john)
+      @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: john)
+    end
 
-  scenario "A user lists all articles" do
-  visit "/"
-  click_link @article.title
-  expect(page).to have_content(@article.title)
-  expect(page).to have_content(@article.body)
-  expect(current_path).to eq(article_path(@article))
+    scenario "A user lists all articles" do
+    visit "/"
+    click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    end
   end
 end
